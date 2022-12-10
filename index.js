c2da268d83c0ac577ebfa731b6821341387b5637
kheersagar <santparja@gmail.com>
2022-12-10 15:42:46 +0530

    initial

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const database = require("./mongoDB_connection");
 
+app.get("/",(req,res)=>{
+  res.send("Server running")
+})
+
 const PORT = 5000 || process.env.PORT
 app.listen(PORT,()=>{
   console.log("-------------server started on port " + PORT + " -----------------")
