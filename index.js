d9e97127381980196bd95317b72615e1de907fc4
kheersagar <santparja@gmail.com>
2023-01-30 10:44:36 +0530

    added contact

--- index.js
+++ index.js
@@ -16,6 +16,59 @@ app.get("/",(req,res)=>{
   res.send("Server running")
 })
 
+// contact
+app.post("/contact",async (reqw,res)=>{
+  try{
+
+    const {email} = req.body
+    var mailOptions = {
+      from: "rahhar848@gmail.com",
+      to: email,
+      subject: "Project Discussion",
+      html: `<div>
+          <h1>Hello ${email}!</h1>
+          <h2>Thanks for contacting us!</h2>
+          <h2>W'll reach back to you soon.</h2>
+        </div>
+      `,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      console.log("called");
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        return true;
+      }
+    });
+    var mailOptions = {
+      from: "rahhar848@gmail.com",
+      to: 'santparja@gmail.com',
+      subject: "Porject Discussion",
+      html: `<div>
+          <h1>Contact details</h1>
+          <h3>email - ${email}, name - ${fullName}, phone - ${phone}</h3>
+          <h3>message - ${message} </h3>
+        </div>
+      `,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      console.log("called");
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        return true;
+      }
+    });
+    res.status(200).send("Congrats!! Your Query is sent")
+  }catch(err){
+    console.log(err.message)
+    res.status(500).send("Internal server error")
+  }
+})
+// 
+
 app.use("/auth",authRouter)
 app.use(tokenCheck)
 app.use("/user",userRouter)
